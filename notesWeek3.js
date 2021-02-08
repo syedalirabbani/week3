ca1535ef24c72840a664f6f5ab60de74ff3e9c80
syedalirabbani <77966033+syedalirabbani@users.noreply.github.com>
2021-02-08 22:33:30 +0800

    Add files via upload

---
+++ notesWeek3.js
@@ -0,0 +1,8 @@
+// random student picker
+
+var students = ["Syed", "Rahimi", "Ashraf", "Ashraf2"];
+
+var randomNo = Math.floor(Math.random() * students.length)
+
+console.log("student who will share screen is " + students[randomNo])
+
