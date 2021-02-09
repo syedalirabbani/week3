656e995ee1f04ad7439a13a23f1baa3ce0a785bd
syedalirabbani <77966033+syedalirabbani@users.noreply.github.com>
2021-02-09 14:58:26 +0800

    Add files via upload

--- notesWeek3.js
+++ notesWeek3.js
@@ -6,3 +6,33 @@ var randomNo = Math.floor(Math.random() * students.length)
 
 console.log("student who will share screen is " + students[randomNo])
 
+/notes ||
+var myString = "Asia Developer Academy"
+
+console.log(myString.length)
+console.log(myString.toUpperCase());
+console.log(myString.toLowerCase());
+console.log(myString.charAt(6));
+console.log(myString.indexOf('ev'));
+console.log(myString.lastIndexOf('e'));
+console.log(myString.substring(5,14));
+console.log(myString.split(" "));
+
+//number methods
+
+var newNum = 12.3456789
+
+console.log(newNum.toFixed(3));
+console.log(newNum.toPrecision(3));
+
+//Math methods
+
+console.log(Math.PI);
+console.log(Math.cos(30));
+
+var number = 12.3
+console.log(Math.floor(number));
+console.log(Math.ceil(number));
+console.log(Math.round(number));
+console.log(Math.random(number));
+  
