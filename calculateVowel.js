660e794ca6b3b6df7f9f7ba66dc4d4932d83cdbf
Syed Ali Rabbani <77966033+syedalirabbani@users.noreply.github.com>
2021-03-09 16:02:55 +0800

    Update calculateVowel.js

--- calculateVowel.js
+++ calculateVowel.js
@@ -3,7 +3,7 @@ function calculationVowel(sentence){
   var answer = 0;
   
   for (var i = 0; i < sentence.length; i++){
-    if(sentence.charAt(i) == 'a' || sentence.charAt(i) == 'c' || sentence,charAt(i) == 'o' || sentence.charAt(i) == 'i' || sentence.charAt(i) == 'u'){
+    if(sentence.charAt(i) == 'a' || sentence.charAt(i) == 'c' || sentence.charAt(i) == 'o' || sentence.charAt(i) == 'i' || sentence.charAt(i) == 'u'){
       answer++
     }
   }
